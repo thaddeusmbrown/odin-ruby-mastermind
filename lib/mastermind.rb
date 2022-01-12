418ae7a689a805f99a33daf34dc9d5d60676a49f
thaddeusmbrown <thaddeusmbrown@gmail.com>
2022-01-12 14:20:47 -0600

    improved computer guessing.  It can sometimes win now, but it has a lot of trouble putting the colors in order once it knows what the four colors are.  Debugged a lot, had issues with clue generation due to array comparison mismatches between guesses and the code causing the number of black and white pegs to be incorrect for certain edgecase guesses

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -17,15 +17,18 @@ def replay
 end
 
 class Game
+  COLOR_CHOICES = ['b', 'g', 'o', 'p', 'r', 'y']
   def initialize
     @board = Board.new
     @player_1 = nil
     @player_2 = nil
+    @guess_set = COLOR_CHOICES.repeated_permutation(4).map(&:join)
   end
 
   def play
     create_player(1)
     create_player(2)
+
     if @player_1.side == "code"
       coder = 1
       coder_type = @player_1.player_type
@@ -37,17 +40,24 @@ class Game
       guesser = 1
       guesser_type = @player_1.player_type
     end
-    # binding.pry
-    code = Display.prompt_code_entry(coder, coder_type)
+    # # # binding.pry
+    code = Display.prompt_code_entry(coder, coder_type, 'code', @guess_set)
     turn_count = 1
     loop do
       @board.show_board
-      guess = Display.prompt_code_entry(guesser, guesser_type)
-      # binding.pry
+      puts @guess_set.include? code.join()
+      puts @guess_set.length
+      p code
+      # # binding.pry
+      guess = Display.prompt_code_entry(guesser, guesser_type, 'guess', @guess_set[0].split(''))
+      ## binding.pry
       temp_code = Array.new(code)
       @board.update_board(guess, temp_code, turn_count)
       if @board.check_victory(turn_count, guesser)
         break
+      elsif guesser_type == 'computer'
+        # binding.pry
+        @guess_set = @board.refine_guesses(@guess_set, turn_count)
       end
       turn_count += 1
     end
@@ -61,7 +71,7 @@ class Game
     else
       side = @player_1.side == "guess" ? "code" : "guess"
     end
-    # binding.pry
+    # # # binding.pry
     if player_number == 1
       @player_1 = Player.new(name, player_type, side)
     else
@@ -91,17 +101,22 @@ class Board
     @board[turn_count][0] = guess
     temp_guess = Array.new(guess)
     @board[turn_count][1] = temp_guess.each_with_index.reduce([]) do |arr, (element, index)|
-      binding.pry
+      # binding.pry
       if temp_code[index] == element
         temp_code[index] = '-'
         temp_guess[index] = ''
         arr.push('b')
-      elsif index == 3 && (temp_guess & temp_code != [])
-        (0...(temp_guess & temp_code).length).each do
-          arr.push('w')
+      end
+      if index == 3 # need to fix this for r y b b
+        temp_guess.each do |remaining_element|
+          if temp_code.include? remaining_element
+            arr.push('w')
+            temp_code.delete_at(temp_code.index(remaining_element))
+          end
         end
-      else
-        arr.push('x')
+        while arr.length < 4
+          arr.push('x')
+        end
       end
       arr.sort()
     end
@@ -116,6 +131,26 @@ class Board
       show_board
       puts "Player #{3-guesser} wins!"
       return 1
+    end
+  end
+
+  def refine_guesses(guess_set, turn_count)
+    # binding.pry
+    guess = @board[turn_count][0]
+    clue = @board[turn_count][1]
+    guess_set.select do |element|
+      if element == 'ygbo' || element == 'bgpo'
+        # binding.pry
+      end
+      temp_guess = Array.new(guess)
+      difference = 4 - element.split('').each_with_index.reduce(0) do |sum, (color, index)|
+        if temp_guess.include? color
+          temp_guess.delete_at(temp_guess.index(color))
+          sum += 1
+        end
+        sum
+      end
+      difference >= clue.count('x') && 4 - difference >= clue.count('b') + clue.count('w') && element.split('') != guess
     end
   end
 end
@@ -167,8 +202,8 @@ class Display
     end
   end
 
-  def self.prompt_code_entry(player_number, player_type)
-    # binding.pry
+  def self.prompt_code_entry(player_number, player_type, code_type, computer_guess)
+    # # binding.pry
     if player_type == "human"
       puts <<-HEREDOC
 
@@ -192,12 +227,14 @@ class Display
           puts "Try again.  Letters must all be in given list and array of length 4"  
         end
       end
-    else
+    elsif code_type == 'code'
       code = []
       (0..3).each do |index|
         code.push(COLOR_CHOICES[rand(0..5)])
       end
       return code
+    else
+      computer_guess
     end
   end
 
